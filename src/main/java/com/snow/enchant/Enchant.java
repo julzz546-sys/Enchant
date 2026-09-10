2e0acb9ecfd1417cace2918bb2f7ef08766c0e62
julzz546-sys <julzz546-sys@users.noreply.github.com>
2026-09-10 17:30:38 +0700

    feat: customize enchantment sources and fishing

---
+++ src/main/java/com/snow/enchant/Enchant.java
@@ -0,0 +1,13 @@
+package com.snow.enchant;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Enchant implements ModInitializer {
+
+    public static final String MOD_ID = "enchant";
+
+    @Override
+    public void onInitialize() {
+        System.out.println("Enchant loaded.");
+    }
+}
